a1240158fad4a2481db3d8881e97f4a7760714c9
nathan <codejourney2022@gmail.com>
2022-04-05 16:50:27 -0400

    Add comments
    Explains how the game will work as code.

--- assets/js/rock_paper_scissors.js
+++ assets/js/rock_paper_scissors.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+/*
+How will the user give input?
+◦ Use the prompt() function, and save the user’s answer to a variable. 
+    ▪ That variable will store the user’s choice for each round.
+
+• How will the computer choose a move?
+◦ Using a function that generates a computer’s move using Math.floor(Math.random.()…) functions.
+    ▪ If the computer generates 1, then return the string “rock”;
+    ▪ If cpu generates 2, then return the string “paper”;
+    ▪ If cpu generates 3, then return the string “scissors”;
+
+• How many rounds will there be?
+◦ 3 rounds.
+◦ Each round will complete after the computer and user choices are compared, with the results being saved to a variable.
+    ▪ “userPoints”
+    ▪ “computerPoints”
+        • When either the userPoints or computerPoints variable value reaches 3, then the game is over and a winner is chosen.
+*/
